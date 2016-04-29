141e9f0ffe42ee680d5577daca1145f8f94ddc6c
Guy <guypham@hotmail.com>
2016-04-28 20:54:34 -0700

    Syncing final project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// This waits till the page finishes loading before running the code inside the {}
+// $(document).ready(function() {
+
+  console.log("If you see this message, jQuery is working.")
+
+  $(document).ready(function(){
+    $('.bxslider').bxSlider({
+      slideWidth: 450
+    });
+  });
+
+
+
+  // $(function(){
+  //       $("#slides").slidesjs({
+  //         width: 700,
+  //         height: 393
+  //
+  //       });
+  //     });
+
+// });
